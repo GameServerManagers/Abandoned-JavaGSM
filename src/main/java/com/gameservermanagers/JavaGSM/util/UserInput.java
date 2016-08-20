7713f87cac449bac092a32aceab491dcec9bc69d
Scarsz <Scarsz@users.noreply.github.com>
2016-08-20 05:24:42 -0700

    user input utility

---
+++ src/main/java/com/gameservermanagers/JavaGSM/util/UserInput.java
@@ -0,0 +1,49 @@
+package com.gameservermanagers.JavaGSM.util;
+
+import org.apache.commons.lang.StringUtils;
+
+import java.util.List;
+
+@SuppressWarnings("unused")
+public class UserInput {
+
+    public static boolean questionYesNo(String message) {
+        String response = "";
+        while (!response.toLowerCase().startsWith("y") && !response.toLowerCase().startsWith("n")) {
+            System.out.print(message.endsWith(" ") ? message : message + " ");
+            response = System.console().readLine();
+        }
+
+        return response.toLowerCase().startsWith("y");
+    }
+
+    public static int questionInt(String message) {
+        while (true) {
+            System.out.print(message.endsWith(" ") ? message : message + " ");
+            String response = System.console().readLine();
+
+            try {
+                return Integer.parseInt(response);
+            } catch (Exception ignored) {}
+        }
+    }
+
+    public static String questionString(String message) {
+        System.out.print(message.endsWith(" ") ? message : message + " ");
+        return System.console().readLine();
+    }
+
+    public static int questionList(String message, List<String> options) {
+        String response = "";
+        while (!StringUtils.isNumeric(response) || Integer.parseInt(response) < 1 || Integer.parseInt(response) > options.size()) {
+            for (String option : options) {
+                System.out.println((options.indexOf(option) + 1) + ": " + option);
+            }
+            System.out.print(message.endsWith(" ") ? message : message + " ");
+            response = System.console().readLine();
+        }
+
+        return Integer.parseInt(response);
+    }
+
+}
