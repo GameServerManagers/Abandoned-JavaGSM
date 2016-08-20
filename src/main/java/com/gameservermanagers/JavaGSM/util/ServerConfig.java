9860dc9eb8983cf74c1636b93af2402e8534f6b7
Scarsz <Scarsz@users.noreply.github.com>
2016-08-20 16:45:36 -0700

    more server config stuff

--- src/main/java/com/gameservermanagers/JavaGSM/util/ServerConfig.java
+++ src/main/java/com/gameservermanagers/JavaGSM/util/ServerConfig.java
@@ -3,8 +3,10 @@ package com.gameservermanagers.JavaGSM.util;
 import java.util.HashMap;
 import java.util.Map;
 
+@SuppressWarnings({"WeakerAccess", "unused"})
 public class ServerConfig {
 
+    //region static stuff
     public static Map<String, String> defaultCommandLines = new HashMap<String, String>(){{
         put("minecraft", "java -Xmx{MEMORY} -jar {JARFILE} nogui");
     }};
@@ -14,12 +16,23 @@ public class ServerConfig {
                 .replace("{JARFILE}", jarFile)
         );
     }
+    //endregion
 
     private String commandLine;
-
+    public String getCommandLine() {
+        return commandLine;
+    }
     public ServerConfig setCommandLine(String commandLine) {
         this.commandLine = commandLine;
         return this;
     }
 
+    private boolean updateOnStart;
+    public boolean getUpdateOnStart() {
+        return updateOnStart;
+    }
+    public ServerConfig setUpdateOnStart(boolean updateOnStart) {
+        this.updateOnStart = updateOnStart;
+        return this;
+    }
 }
