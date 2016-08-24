97e7c6ac6bce18eca4f9b8f11df0e87858b31579
Scarsz <Scarsz@users.noreply.github.com>
2016-08-24 15:41:54 -0700

    specify executing directory for RuntimeUtil

--- src/main/java/com/gameservermanagers/JavaGSM/util/RuntimeUtil.java
+++ src/main/java/com/gameservermanagers/JavaGSM/util/RuntimeUtil.java
@@ -1,13 +1,18 @@
 package com.gameservermanagers.JavaGSM.util;
 
+import java.io.File;
 import java.io.IOException;
 
+@SuppressWarnings("WeakerAccess")
 public class RuntimeUtil {
 
     public static Process runProcess(String command) {
+        return runProcess(command, new File("."));
+    }
+    public static Process runProcess(String command, File directory) {
         try {
             System.out.println("Running command \"" + command + "\"");
-            return Runtime.getRuntime().exec(command);
+            return Runtime.getRuntime().exec(command, null, directory);
         } catch (IOException e) {
             e.printStackTrace();
             return null;
@@ -15,8 +20,11 @@ public class RuntimeUtil {
     }
 
     public static Process runProcessWaitFor(String command) {
+        return runProcessWaitFor(command, new File("."));
+    }
+    public static Process runProcessWaitFor(String command, File directory) {
         try {
-            Process p = runProcess(command);
+            Process p = runProcess(command, directory);
             if (p != null) p.waitFor();
             return p;
         } catch (InterruptedException e) {
