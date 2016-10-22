7951a90f1a64171550cb5fac4c7ba1998d57654c
Scarsz <Scarsz@users.noreply.github.com>
2016-10-22 16:53:47 -0700

    organize DownloadUtil, add deleteFile() & getUrlAsString()

--- src/main/java/com/gameservermanagers/JavaGSM/util/DownloadUtil.java
+++ src/main/java/com/gameservermanagers/JavaGSM/util/DownloadUtil.java
@@ -1,10 +1,12 @@
 package com.gameservermanagers.JavaGSM.util;
 
+import com.gameservermanagers.JavaGSM.JavaGSM;
 import net.lingala.zip4j.core.ZipFile;
 import net.lingala.zip4j.exception.ZipException;
 import org.apache.commons.io.FileUtils;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
+import org.jsoup.Jsoup;
 import org.rauschig.jarchivelib.ArchiverFactory;
 
 import java.io.File;
@@ -13,6 +15,27 @@ import java.net.URL;
 
 @SuppressWarnings({"WeakerAccess", "unused"})
 public class DownloadUtil {
+
+    public static void clone(String uri) {
+        clone(uri, new File("."));
+    }
+    public static void clone(String uri, File destination) {
+        System.out.print("Cloning " + uri + " into " + destination + "...");
+        long startTime = System.currentTimeMillis();
+        try { Git.cloneRepository().setURI(uri).setDirectory(destination).call(); } catch (GitAPIException e) { e.printStackTrace(); }
+        System.out.println(" done in " + ((System.currentTimeMillis() - startTime)/1000L) + " seconds; " + FileUtils.sizeOfDirectory(destination)/1024L/1024L + "MB");
+    }
+
+    public static void deleteFile(File fileToDelete) {
+        System.out.print("Deleting file \"" + fileToDelete.getAbsolutePath() + "\"... ");
+        if (!fileToDelete.exists()) {
+            System.out.println("failed. File doesn't exist.");
+        } else if (!fileToDelete.delete()) {
+            System.out.println("failed. Permissions!?");
+        } else {
+            System.out.println(fileToDelete.exists() ? "failed." : "success");
+        }
+    }
 
     public static void download(String url) {
         String[] split = url.split("/");
@@ -23,6 +46,36 @@ public class DownloadUtil {
         long startTime = System.currentTimeMillis();
         try { FileUtils.copyURLToFile(new URL(url), destination); } catch (IOException e) { e.printStackTrace(); }
         System.out.println(" done in " + ((System.currentTimeMillis() - startTime)/1000L) + " seconds; " + destination.length()/1024L/1024L + "MB");
+    }
+
+    public static String getUrlAsString(String url) {
+        return getUrlAsString(url, "JavaGSM v" + JavaGSM.version);
+    }
+    public static String getUrlAsString(String url, String userAgent) {
+        try {
+            String response = Jsoup
+                    .connect(url)
+                    .userAgent(userAgent)
+                    .ignoreContentType(true)
+                    .get()
+                    .html()
+            ;
+            if (response.startsWith("<html>\n <head></head>\n <body>\n  ")) response = response.substring(32);
+            if (response.endsWith("\n </body>\n</html>")) response = response.substring(0, response.length() - 17);
+            return response;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void moveAllChildrenOfFolderToParent(File target) {
+        moveAllChildrenOfFolderToParent(target, false);
+    }
+    public static void moveAllChildrenOfFolderToParent(File target, boolean deleteTarget) {
+        for (File file : target.listFiles())
+            file.renameTo(new File(file.getParentFile().getParentFile(), file.getName()));
+        if (deleteTarget) target.delete();
     }
 
     public static void unzip(File source) {
@@ -60,25 +113,6 @@ public class DownloadUtil {
         }
 
         System.out.println(" done in " + (System.currentTimeMillis() - startTime) + "ms");
-    }
-
-    public static void clone(String uri) {
-        clone(uri, new File("."));
-    }
-    public static void clone(String uri, File destination) {
-        System.out.print("Cloning " + uri + " into " + destination + "...");
-        long startTime = System.currentTimeMillis();
-        try { Git.cloneRepository().setURI(uri).setDirectory(destination).call(); } catch (GitAPIException e) { e.printStackTrace(); }
-        System.out.println(" done in " + ((System.currentTimeMillis() - startTime)/1000L) + " seconds; " + FileUtils.sizeOfDirectory(destination)/1024L/1024L + "MB");
-    }
-
-    public static void moveAllChildrenOfFolderToParent(File target) {
-        moveAllChildrenOfFolderToParent(target, false);
-    }
-    public static void moveAllChildrenOfFolderToParent(File target, boolean deleteTarget) {
-        for (File file : target.listFiles())
-            file.renameTo(new File(file.getParentFile().getParentFile(), file.getName()));
-        if (deleteTarget) target.delete();
     }
 
 }
