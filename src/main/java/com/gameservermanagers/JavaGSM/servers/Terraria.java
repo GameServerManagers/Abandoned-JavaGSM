6004673a247e6fc650640c7d0c8fb4df84a52e17
DrPiggy <idan.horowitz@gmail.com>
2016-10-23 11:19:04 +0300

    make Terraria download use TShock/releases/latest

--- src/main/java/com/gameservermanagers/JavaGSM/servers/Terraria.java
+++ src/main/java/com/gameservermanagers/JavaGSM/servers/Terraria.java
@@ -1,23 +1,24 @@
 package com.gameservermanagers.JavaGSM.servers;
 
+import com.gameservermanagers.JavaGSM.JavaGSM;
 import com.gameservermanagers.JavaGSM.ServerInstaller;
 import com.gameservermanagers.JavaGSM.util.DownloadUtil;
+import com.google.gson.internal.LinkedTreeMap;
 
 import java.io.File;
+import java.util.List;
 
 public class Terraria implements ServerInstaller {
 
     public static void install(File destination) {
         System.out.print("Obtaining latest download url...");
-        //TODO: switch this to use github api at https://api.github.com/repos/NyxStudios/TShock/releases/latest
-        String latestDownloadUrl = "https://github.com" + DownloadUtil.getUrlAsString("https://github.com/NyxStudios/TShock/releases/latest").split("<ul class=\"release-downloads\">")[1].split("<a href=\"")[1].split("\"")[0];
-        System.out.println(" " + latestDownloadUrl);
-
         File tShockArchive = new File(destination, "tshock.zip");
-        DownloadUtil.download(latestDownloadUrl, tShockArchive);
-        DownloadUtil.unzip(tShockArchive);
-        DownloadUtil.deleteFile(tShockArchive);
-
+        List<LinkedTreeMap<String, Object>> assetsFromApi = (List<LinkedTreeMap<String, Object>>) ((LinkedTreeMap<String, Object>) JavaGSM.gson.fromJson(DownloadUtil.getUrlAsString("https://api.github.com/repos/NyxStudios/TShock/releases/latest"), LinkedTreeMap.class)).get("assets");
+        for (LinkedTreeMap<String, Object> asset : assetsFromApi) {
+            DownloadUtil.download((String) asset.get("browser_download_url"), tShockArchive);
+            DownloadUtil.unzip(tShockArchive);
+            DownloadUtil.deleteFile(tShockArchive);
+        }
         System.out.println("Finished installing TShock server. Start it with the -s flag.");
     }
 
