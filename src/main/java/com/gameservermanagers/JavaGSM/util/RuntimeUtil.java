a2598c5f28e894ba7bbb859611af15a4b9e877de
SSederberg <crash_1703@yahoo.com>
2017-02-14 17:11:34 -0600

    Remove debug line

--- src/main/java/com/gameservermanagers/JavaGSM/util/RuntimeUtil.java
+++ src/main/java/com/gameservermanagers/JavaGSM/util/RuntimeUtil.java
@@ -14,7 +14,7 @@ public class RuntimeUtil {
     }
     public static Process runProcess(String command, File directory, boolean gobble) {
             try {
-            System.out.println("Running command \"" + command + "\" in " + directory.getAbsolutePath());
+            System.out.println("Running command \"" + command + "\"");
             Process p = Runtime.getRuntime().exec(command, null, directory);
 
             if (p != null && gobble) {
